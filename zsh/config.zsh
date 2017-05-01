077cbb3c2e7db3a5d930ae6356b0fd2023f90bd1
doener <doenerkebap@mailbox.org>
2017-05-01 23:36:22 +0200

    Add basic zsh config.
    A mix from zsh's first-start-wizzard and @holman's config.

---
+++ zsh/config.zsh
@@ -0,0 +1,18 @@
+HISTFILE=~/.zsh_history        # history filename
+HISTSIZE=10000                 # ?
+SAVEHIST=10000                 # ?
+setopt APPENDHISTORY           # add history
+setopt EXTENDED_HISTORY        # add timestamps to history
+setopt HIST_IGNORE_ALL_DUPS    # no duplicates
+setopt HIST_REDUCE_BLANKS      # no superfluous white space
+setopt SHARE_HISTORY           # import from and append to history file
+
+setopt CORRECT                 # correct spelling
+setopt BEEP NOMATCH            # ?
+
+setopt COMPLETE_ALIASES        # prevents aliases from being internally substituted before completion is attempted.
+setopt COMPLETE_IN_WORD        # cursor stays at position after completion
+
+setopt PROMPT_SUBST            #  allow parameter expansion, command substitution and arithmetic expansion in prompt
+
+bindkey -v                     # ?
