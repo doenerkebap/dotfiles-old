72f6593176bcb17e4c4d6a953e1a9c7b51781492
doener <doenerkebap@mailbox.org>
2017-05-01 23:38:06 +0200

    Add zsh aliases.

---
+++ zsh/aliases.zsh
@@ -0,0 +1,13 @@
+alias ll='ls -l'
+alias la='ls -A'
+alias l.='ls -d .*'
+
+alias ..='cd ..'
+
+alias python=python3
+alias ipython=ipython3
+alias pip=pip3
+
+alias rubber='rubber --inplace --maxerr -1 --pdf --short --warn all'
+
+alias tmux='tmux -2'
