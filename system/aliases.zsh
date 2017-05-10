a4f2e109ac5b3dfbba03e4b2062198afddefb7e2
doener <doenerkebap@mailbox.org>
2017-05-10 23:29:05 +0200

    Add system aliases q, c, fm, cd.., cp, rm

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,6 +1,15 @@
+alias q='exit'
+alias c='clear'
+alias fm='ranger'
+
 alias ll='ls -l'
 alias la='ls -A'
 alias lla='ls -lA'
 alias l.='ls -d .*'
 
+alias cd..="cd .."
 alias ..='cd ..'
+
+alias cp='cp -i'
+alias rm='rm -i'
+
