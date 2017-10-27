b2596b2e94a36463f607acb4ad0adfe1e55a8c33
doener <doenerkebap@mailbox.org>
2017-10-27 21:22:25 +0200

    Add rofi.

--- i3/i3.symlink/status.py
+++ i3/i3.symlink/status.py
@@ -1,122 +1,98 @@
+"""This file configures the statusline shown i3 bar."""
 from i3pystatus import Status
+from i3pystatus.core.command import run_through_shell
 
-color_good = "#859900"
-color_degraded = "#b58900"
-color_bad = "#dc322f"
+COLOR_GOOD = "#859900"
+COLOR_DEGRADED = "#b58900"
+COLOR_NEUTRAL = "#ffffff"
+COLOR_BAD = "#dc322f"
 
 status = Status()
 
-# Displays clock like this:
-# Tue 30 Jul 11:59:46 PM KW31
-#                          ^-- calendar week
-# status.register("clock",
-#         format="%a %-d %b %H:%M:%S KW%V",)
+# Date / Clock
+# Tue 30.10l 14:59:46 KW31
+status.register("clock",
+    format="%a %-d.%m. %H:%M:%S KW%V",)
 
-# Shows the average load of the last minute and the last 5 minutes
-# (the default value for format is used)
-status.register("load")
-
-status.register("cpu_usage")
-
-# Shows your CPU temperature, if you have a Intel CPU
-status.register("temp",
-    format="{temp:.0f}°C",)
-
-# CPU freq
-status.register("cpu_freq",
-    format="{avgg} GHz",)
-
-
-
-# The battery monitor has many formatting options, see README for details
-
-# This would look like this, when discharging (or charging)
-# ↓14.22W 56.15% [77.81%] 2h:41m
-# And like this if full:
-# =14.22W 100.0% [91.21%]
-#
-# This would also display a desktop notification (via D-Bus) if the percentage
-# goes below 5 percent while discharging. The block will also color RED.
-# If you don't have a desktop notification demon yet, take a look at dunst:
-#   http://www.knopwob.org/dunst/
+# Battery
 status.register("battery",
-    format="BAT {status} {consumption:.2f}W [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
+    format="[{status} ]{consumption:.1f} W [{percentage:.0f} %] {remaining:%E%hh:%Mm}",
     alert=True,
     alert_percentage=5,
     status={
-        "DIS": "↓",
-        "CHR": "↑",
-        "FULL": "=",
-    },)
+        "DIS": "▼",
+        "CHR": "▲",
+        "FULL": "",
+    },
+    full_color=COLOR_GOOD,
+    critical_color=COLOR_BAD,
+    charging_color=COLOR_GOOD,
+    )
 
+# System load
+status.register("load",
+    critical_color=COLOR_BAD)
 
-status.register("backlight",
-        format="{percentage}%",
-        base_path="/sys/class/backlight/intel_backlight/",
-        on_leftclick="lighter",
-        on_rightclick="darker")
+# CPU usage
+status.register("cpu_usage")
 
-# This would look like this:
-# Discharging 6h:51m
-# status.register("battery",
-#     format="{status} {remaining:%E%hh:%Mm}",
-#     alert=True,
-#     alert_percentage=5,
-#     status={
-#         "DIS":  "Discharging",
-#         "CHR":  "Charging",
-#         "FULL": "Bat full",
-#     },)
+# CPU Temperature
+status.register("temp",
+        format="{temp:.0f} °C",)
 
-# Shows the address and up/down state of eth0. If it is up the address is shown in
-# green (the default value of color_up) and the CIDR-address is shown
-# (i.e. 10.10.10.42/24).
-# If it's down just the interface name (eth0) will be displayed in red
-# (defaults of format_down and color_down)
-#
+# CPU frequency
+status.register("cpu_freq",
+    format="{avgg} GHz",)
+
+# Wired network
 # Note: the network module requires PyPI package netifaces
 status.register("network",
     interface="enp0s25",
     format_up="{v4cidr}",
-    color_up=color_good,
-    color_down=color_bad)
+    format_down="{interface}:",
+    color_up=COLOR_GOOD,
+    color_down=COLOR_BAD)
 
+# Wireless network
 # Note: requires both netifaces and basiciw (for essid and quality)
 status.register("network",
     interface="wlp3s0",
-    format_up="{essid} {quality:2.0f}% {v4cidr}",
-    color_up=color_good,
-    color_down=color_bad)
+    format_up="{essid} {quality:2.0f} % {v4cidr} {kbs} kiB/s",
+    format_down="{interface}: ",
+    dynamic_color=False,
+    color_up=COLOR_GOOD,
+    color_down=COLOR_BAD)
 
 # Shows disk usage of /
-# Format:
-# 42/128G [86G]
 status.register("disk",
     path="/",
-    format="{avail} GiB",)
+    format="{avail:.1f} GiB",
+    critical_color=COLOR_BAD,
+    )
 
 status.register("mem",
-        color=color_good,
-        warn_color=color_degraded,
-        alert_color=color_bad)
+        format="{avail_mem:.1f} GiB",
+        divisor=1073741824,
+        color=COLOR_GOOD,
+        warn_color=COLOR_DEGRADED,
+        alert_color=COLOR_BAD)
 
+# Sound volume
 status.register("alsa",
-        format="VOL {volume}")
-# Shows pulseaudio default sink volume
-#
-# Note: requires libpulseaudio from PyPI
-# status.register("pulseaudio",
-    # format="♪{volume}",)
+        format="{volume} %")
 
-# Shows mpd status
-# Format:
-# Cloud connected▶Reroute to Remain
-# status.register("mpd",
-#     format="{title}{status}{album}",
-#     status={
-#         "pause": "▷",
-#         "play": "▶",
-#         "stop": "◾",
-#     },)
+def backlight_up():
+    run_through_shell(["xbacklight", "-inc", 10])
+
+def backlight_down():
+    run_through_shell(["xbacklight", "-dec", 10])
+
+# Backlight
+status.register("backlight",
+        format="{percentage} %",
+        base_path="/sys/class/backlight/intel_backlight/",
+        on_leftclick=backlight_up,
+        on_rightclick=backlight_down)
+
 
 status.run()
